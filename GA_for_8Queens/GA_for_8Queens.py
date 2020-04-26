efec6b9d1e45655049221d967d66cdb2d82ded3d
AxeXiang <1015806421@qq.com>
2020-04-26 22:44:44 +0800

    update GA for 8Queens

--- GA_for_8Queens/GA_for_8Queens.py
+++ GA_for_8Queens/GA_for_8Queens.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 """
 Genetic Algorithm to solve 8 Queens problem
-unfinished, to be continue...
+Obviously this is not an effective method,
+I just try to solve it with genetic algorithm.
 """
 
 import numpy as np
@@ -43,16 +44,46 @@ class GA8Queen:
             all_fitness.append(parent_fitness)
         return np.array(all_parents), np.array(all_fitness)
 
-    def evolve(self, tournament_size):
-        all_parents, fitness = self.tournament(tournament_size)
-        if 0 in fitness:
-            zero_position = [i for i, v in enumerate(fitness) if v == 0]
-            perfect_state = np.unique(all_parents[zero_position], axis=0)
-            self.all_state.append(perfect_state)
+    def record_and_replace(self, all_parents, all_fitness):
+        if 0 in all_fitness:
+            zero_position = [i for i, v in enumerate(all_fitness) if v == 0]
+            perfect_state = np.unique(all_parents[zero_position], axis=0).tolist()
+            for one_state in perfect_state:
+                if one_state not in self.all_state:
+                    self.all_state.append(one_state)
             all_parents = np.delete(all_parents, zero_position, axis=0)
-            print(len(all_parents))
+            add_pop = np.vstack([np.random.permutation(self.queen_number) for _ in range(len(zero_position))])
+            all_parents = np.append(all_parents, add_pop, axis=0)
+        return all_parents
 
+    def crossover(self, parent, all_parents):
+        if np.random.rand() < self.cross_rate:
+            i_ = np.random.randint(0, self.pop_size, size=1)
+            cross_points = np.random.randint(0, 2, self.queen_number).astype(np.bool)
+            parent[cross_points] = all_parents[i_, cross_points]
+        return parent
+
+    def mutate(self, child):
+        for point in range(self.queen_number):
+            if np.random.rand() < self.mutate_rate:
+                swap_point = np.random.randint(0, self.queen_number)
+                swapA, swapB = child[point], child[swap_point]
+                child[point], child[swap_point] = swapB, swapA
+        return child
+
+    def evolve(self, tournament_size, generation):
+        for n in range(generation):
+            all_parents, all_fitness = self.tournament(tournament_size)
+            all_parents = self.record_and_replace(all_parents, all_fitness)
+            all_parents_copy = all_parents.copy()
+            for parent in all_parents:
+                child = self.crossover(parent, all_parents_copy)
+                child = self.mutate(child)
+                parent[:] = child
+            self.pop = all_parents
+            print(self.all_state)
+            print(len(self.all_state))
 
 
 if __name__ == '__main__':
-    GA8Queen(queen_number=8, pop_size=100, cross_rate=0.4, mutate_rate=0.2).evolve(20)
+    GA8Queen(queen_number=6, pop_size=1000, cross_rate=0.1, mutate_rate=0.4).evolve(20, 200)
